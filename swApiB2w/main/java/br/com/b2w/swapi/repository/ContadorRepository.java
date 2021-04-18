b375feb27a7f3891ea459928deb422b5c1aa542e
Jonathan Philipe <sicrano3000@gmail.com>
2021-04-18 19:10:14 -0300

    Delete swApiB2w/main directory

--- swApiB2w/main/java/br/com/b2w/swapi/repository/ContadorRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.b2w.swapi.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import br.com.b2w.swapi.model.Contador;
-
-public interface ContadorRepository extends MongoRepository<Contador, String> {
-
-}
